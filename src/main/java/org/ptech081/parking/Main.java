2dc9348acc43b76d706fac4c869aff99a77f8f0f
unknown <Aprendiz@BOGDFPCGMS4130.FormacionSena.RED>
2024-02-16 10:21:11 -0500

    Registro de E/S completos

--- src/main/java/org/ptech081/parking/Main.java
+++ src/main/java/org/ptech081/parking/Main.java
@@ -1,5 +1,10 @@
 package org.ptech081.parking;
 import org.ptech081.parking.entities.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.ptech081.parking.entities.Carro;
 
@@ -19,13 +24,41 @@ public class Main {
 //invocar el metodo addCar 
         cliente1.addCar(carrito1);
         cliente1.addCar("SAW 090", TipoVehiculo.MOTO);
-
+   
+ 
 //recorrer los carros del cliente 
+    Cupo cupo1 = new Cupo('A');
+    Cupo cupo2 = new Cupo('B');
+    
+///Crear 2 registros E/S
+    LocalDate fechaInicio = LocalDate.now();
+    LocalTime horaInicio = LocalTime.now();
+    LocalDate fechaFinal = LocalDate.of(2024, Month.MARCH, 24);
+    LocalTime horaFinal = LocalTime.of(11 , 30 , 00);
 
-for( Carro c:  cliente1.misCarros ){
-    System.out.println(c.placa + " " +"---"+ "" + c.tipoVehiculo);
+        List<Registro> misRegistros = new ArrayList<Registro>();
+//crear registro
+        Registro registro1 = new Registro( fechaInicio , horaInicio , fechaFinal, horaFinal , 5000L , cupo1 , cliente1.misCarros.get(0) ,cliente1 );
+
+
+
+        LocalDate fechaInicio1 = LocalDate.now();
+        LocalTime horaInicio1 = LocalTime.now();
+        LocalDate fechaFinal1 = LocalDate.of(2024, Month.FEBRUARY, 28);
+        LocalTime horaFinal1 = LocalTime.of(15 , 30 , 00);
     
+        Registro registro2 = new Registro( fechaInicio1 , horaInicio1 , fechaFinal1, horaFinal1 , 3000L , cupo2 , cliente1.misCarros.get(1) ,cliente1 );
+    
+    
+    //añadir el registro a la lista 
+        misRegistros.add(registro1);
+        System.out.println("Registro de E/S parking");
+        for(Registro r : misRegistros){
+           System.out.println("Cliente: " + r.cliente.nombre + " " + r.cliente.apellidos + "|" + r.carro.placa + " " + r.carro.tipoVehiculo + "|" + r.valor + "|" + r.fechaInicio + " " + r.horaInicio + "|" ); 
+        }
+
+
+
 }
-    
+
     }
-}
--- src/main/java/org/ptech081/parking/entities/Registro.java
+++ src/main/java/org/ptech081/parking/entities/Registro.java
@@ -1,5 +1,36 @@
 package org.ptech081.parking.entities;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class Registro {
+    
+///Atributos intrinsecos
+    public LocalDate fechaInicio;
+    public LocalTime horaInicio;
+    public LocalDate fechaFinal;
+    public LocalTime horaFinal;
+    public Long valor;
+
+///Atributos de relacion
+    public Cupo cupo;
+    public Carro carro;
+    public Cliente cliente;
+  
+///Constructor por defecto
+
+    public Registro() {
+    }
+
+    public Registro(LocalDate fechaInicio, LocalTime horaInicio, LocalDate fechaFinal, LocalTime horaFinal, Long valor,
+            Cupo cupo, Carro carro, Cliente cliente) {
+        this.fechaInicio = fechaInicio;
+        this.horaInicio = horaInicio;
+        this.fechaFinal = fechaFinal;
+        this.horaFinal = horaFinal;
+        this.valor = valor;
+        this.cupo = cupo;
+        this.carro = carro;
+        this.cliente = cliente;
+    }
 
 }
